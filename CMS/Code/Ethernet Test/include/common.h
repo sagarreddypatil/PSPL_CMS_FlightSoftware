7ffef347a2776075078826ea155c7e1db7d42847
Sagar Patil <sagarreddypatil@gmail.com>
2022-05-18 21:01:33 -0400

    :eclair:

---
+++ CMS/Code/Ethernet Test/include/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <Ethernet.h>
+
+IPAddress subnetMask(255, 255, 255, 0);
+IPAddress gateway(10, 4, 26, 100);
+
+#endif
---
+++ CMS/Code/Ethernet Test/src/transmitter.cpp
@@ -0,0 +1,13 @@
+#ifdef TRANSMITTER
+
+#include <Arduino.h>
+
+int setup()
+{
+}
+
+int loop()
+{
+}
+
+#endif // TRANSMITTER
