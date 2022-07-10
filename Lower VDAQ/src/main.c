7bd35ab8d5fd2098c31e10870cf93b444ffd08aa
Sagar Patil <sagarreddypatil@gmail.com>
2022-07-10 18:02:42 -0400

    add basic filesystem hierarchy

--- Lower VDAQ/src/main.c
+++ Lower VDAQ/src/main.c
@@ -3,11 +3,14 @@
 // Native Blink Example
 
 #include <avr/io.h>
+#include <example.h>
 #include <util/delay.h>
 
 int main(void) {
   // make the LED pin an output for PORTB5
   DDRB = 1 << 5;
+
+  int fifteen = multiply(5, 3);
 
   while (1) {
     _delay_ms(500);
