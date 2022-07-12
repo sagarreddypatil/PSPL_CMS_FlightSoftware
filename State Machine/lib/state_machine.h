bcf5dd6361c36b3cfefdbd2befcfbaa739f1027d
Cameron <will2267@purdue.edu>
2022-07-12 03:12:20 -0400

    State machine basic implementation pt2

--- State Machine/lib/state_machine.h
+++ State Machine/lib/state_machine.h
@@ -1,15 +1,19 @@
+#include <stdbool.h>
+
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
 
-#define SAFE        0b00000001
-#define SKETCHY     0b00000010
-#define RECORD      0b00000100
-#define FINISHED    0b00001000
+struct StateStruct {
+    bool safe;
+    bool danger;
+    bool record;
+    bool finished;
+} init_state = {true, false, false, false};
 
 // State Byte can encode up to 255 states in a single byte instead of one byte for each state (bool)
-typedef char state_byte;
+typedef struct StateStruct State;
 
 // Useful print function
-void print_state_byte(state_byte);
+void print_state(State);
 
 #endif
--- State Machine/src/state_machine.c
+++ State Machine/src/state_machine.c
@@ -13,77 +13,42 @@ Starting this project was kinda like this: https://youtu.be/vFUx_KC1bHQ?t=77
 
 int main(int argc, char* argv[]) {
     /*
-    Demonstrating how to use the "state_byte" to convey the state of the system
+    Super basic state implementation
 
-    Why are we using this "state_byte"?
-        The state_byte allows us to have 1 byte store up to 8 states instead of using booleans, in which case each state uses 1 byte
-        Basically we are using each bit as a boolean, if we don't wanna do this we should decide soon cause the state machine is a big part lol
-        The rocket can also be in more than one state at the same time and it's easy to do that with the state byte
+    It actually won't ever get much more complicated than this
 
-    Right now, there are only four states to demonstrate using the state byte: safe, sketchy, record and finished
-
-    Here's a small state map for the kids at home:
-
-        <---> means we can go back and forth,
-        ----> means we can only go one way,
-        <===> means we can be in both states at the same time
-
-        SAFE <---> SKETCHY <===> RECORD ----> FINISHED <===> SAFE
-
-        I can make a more detailed one because a one line state map doesn't give great detail on all paths but you get the gist
-
-    The folling are various examples of how to use and manipulate the state byte
-
-    NOTE!!!
-        This is very much thrown together and can be optimized and whatnot so if you don't like the state byte because of this implementation then think again!
+    The logic behind it should always be simple, very important to be able to clearly trace your way from state to state
     */
-
     // Initialize the state byte in the safe state
-    state_byte state = SAFE;
+    State state = init_state;
     
 
-    // This is how you evaluate the state, b
-    if((state & SAFE) == SAFE && (state & FINISHED) != FINISHED) {
-        print_state_byte(state);
+    if(state.safe && !state.finished) {
+        print_state(state);
     }
 
-    // Put the state byte into an invalid state, we'd have logic to get to this point not just a statement like this
-    state += SKETCHY;
-    if((state & SAFE) == SAFE && (state & SKETCHY) == SKETCHY && (state & FINISHED) != FINISHED) {
-        state = SKETCHY;
-        print_state_byte(state);
+    state.danger = true;
+
+    if(state.safe && state.danger) {
+        state.safe = false;
+        state.record = true;
+        print_state(state);
     }
 
-    // Here's a condition where we can be in more than one state, but where there are some states we don't wanna be in
-    // There's a cleaner way to do this don't worry it's just 11PM and I have to wake up early for work in the morning, turn that mindset into a grindset
-    state += RECORD;
-    if((state & RECORD) == RECORD && (state & SAFE) != SAFE && (state & FINISHED) != FINISHED) {
-        print_state_byte(state);
+    state.finished = true;
+
+    if(state.finished) {
+        state.safe = true;
+        state.danger = false;
+        state.record = false;
+        print_state(state);
     }
 
-    // This is an example of entering the finished state where we cannot go back from
-    state += FINISHED;
-    if((state & FINISHED) == FINISHED) {
-        state = FINISHED + SAFE;
-        print_state_byte(state);
-    }
 }
 
-void print_state_byte(state_byte state) {
-    // Yes I know this function is written poorly get over it
-    int reverse = 0;
-
-    for(int rev_idx = 0; rev_idx < sizeof(state) * 8; rev_idx++) {
-        reverse <<= 1;
-        if (state & 1 == 1)
-            reverse ^= 1;
-        state >>= 1;
-    }
-
-    printf("State Byte: ");
-    for(int idx = 0; idx < sizeof(state) * 8; idx++) {
-        printf("%d", reverse % 2);
-        reverse /= 2;
-    }
-    printf("\n");
+void print_state(State state) {
+    printf("\nSafe: %d\n", state.safe);
+    printf("Danger: %d\n", state.danger);
+    printf("Recording: %d\n", state.record);
+    printf("Finished: %d\n\n", state.finished);
 }
