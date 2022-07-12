c556ce63b4c039b7dab5f21591bc73236dd116bf
Cameron <will2267@purdue.edu>
2022-07-11 21:50:15 -0400

    minor changes

---
+++ State Machine/src/state_machine.c
@@ -0,0 +1,16 @@
+/*
+Built by Mr. Dr. Lord Master God Emperor Supreme Chancelor Overlord etc etc etc Big Cameron himself
+
+Here Big Cameron from the planet Earth first started coding CMS, July 10th, 2022
+
+We came in hate for Blue Origin
+
+Starting this project was kinda like this: https://youtu.be/vFUx_KC1bHQ?t=77
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("\nI'm leaving for Vandenburg too soon for me to actually start programming the state machine so here's this placeholder print statement.\n");
+    printf("Here's a good resource on what a state machine is: https://www.itemis.com/en/yakindu/state-machine/documentation/user-guide/overview_what_are_state_machines\n\n")
+}
