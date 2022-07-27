b83ab17410f9edc4fafc00d7c0cdc36cd27b9f66
Cameron <will2267@purdue.edu>
2022-07-27 01:00:50 -0700

    cleaned up the master branch

--- Libraries/example/example.c
+++
@@ -1,6 +0,0 @@
-#include "example.h"
-
-int add(int a, int b) { return a + b; }
-int subtract(int a, int b) { return a - b; }
-int multiply(int a, int b) { return a * b; }
-int divide(int a, int b) { return a / b; }
--- Libraries/example/example.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXAMPLE_H
-#define EXMAPLE_H
-
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b); // very slow on AVR
-
-#endif
