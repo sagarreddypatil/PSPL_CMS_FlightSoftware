af49d8a1b936bc7dc37e92d7645f7c621566c388
Cameron <will2267@purdue.edu>
2022-07-30 16:44:53 -0700

    reviewed

--- Libraries/spi/spi.c
+++ Libraries/spi/spi.c
@@ -67,7 +67,7 @@ void spi_transaction(void *transmit, void *recieve, uint8_t len) {
             SPDR = *(uint8_t*)(transmit + data_idx);
         }
         else {
-            SPDR = 0x00;
+            SPDR = 0xBC;
         }
         // Wait for transmission complete by looking at the SPI interrupt
         while(!SPI_INTERRUPT);
