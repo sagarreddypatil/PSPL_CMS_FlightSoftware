e59d465a45f42ccbb0e8c5881c2e9dba908461e1
Cameron <will2267@purdue.edu>
2022-08-07 15:00:40 -0700

    minor changes

--- ECU/src/main.c
+++ ECU/src/main.c
@@ -1,1 +1,16 @@
 // Big Cameron Was Here
+
+#include "ecu.h"
+#include "uart.h"
+#include "timer.h"
+
+int main() {
+    uart_init(2000000); 
+    uart_printf("Size of data_t: %d\n", sizeof(ecu_data_t));
+    uint64_t finish;
+    ecu_data_t data;
+
+    data.pressure_1 = 0xFFFF;
+    data.pressure_2 = 0xFFFF;
+    data.pressure_3 = 0xFFFF;
+}
