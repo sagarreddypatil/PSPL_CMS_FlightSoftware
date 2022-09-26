d98ab7e25539dd8af5810568ef5d200a24e29867
Sagar Patil <sagarreddypatil@gmail.com>
2022-09-26 18:37:35 -0400

    add clean option to build script

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 
 # run cmake
 cmake -B build
+
+# if clean is passed as an argument, run `cmkae --build build --target clean`
+if [ "$1" = "clean" ]; then
+    cmake --build build --target clean
+fi
+
 cmake --build build
