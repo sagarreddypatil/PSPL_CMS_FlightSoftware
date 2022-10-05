bf1996b3d5aaea165eadb2aa4214faad311b99c7
tj5k <taylorduchinski@gmail.com>
2022-10-05 18:02:39 -0400

    started adding the on off commands but there will be more chnages to them for sure

--- Bang Bang Boom Box/Power_System_Controller_firmware/src/main.cpp
+++ Bang Bang Boom Box/Power_System_Controller_firmware/src/main.cpp
@@ -15,27 +15,150 @@ char input1[INPUT_SIZE1 + 1];
 byte size;  // Size of the incoming command as read from Serial
 
 void read_command();
+void Drok_off();
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);   //start coms with control computer 
-  Serial1.begin(9600);  //start coms with Drok_1 
-  Serial2.begin(9600);  //start coms with Drok_2
-  Serial3.begin(9600);  //start coms with Drok_3
+  Serial1.begin(9600);  //start coms with Drok_0 
+  Serial2.begin(9600);  //start coms with Drok_1
+  Serial3.begin(9600);  //start coms with Drok_2
+
+  //Start with all Droks off 
+  Serial1.println("awo0");
+  delay(50);
+  while (Serial1.available() > 0) {
+    Serial1.read();
+  }
+  Serial2.println("awo0");
+  delay(50);
+  while (Serial2.available() > 0) {
+    Serial2.read();
+  }
+  Serial3.println("awo0");
+  delay(50);
+  while (Serial3.available() > 0) {
+    Serial3.read();
+  }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   read_command();
 
-  if (cmd == 'a'){    //stamtents for each command 
-    Serial.print("im command a statment: ");
 
-    if(cmd_parameter[0] == 1){  //statmenmt to look at the first argument of command 'a' this can be done for up to 10 arguments 
-      //some cool code to do some stuff 
-      Serial.println("1");
+  /* 
+  cmd_parameter[subCommand]
+  o,0 - Drok 0 OutPut on 
+  o,1 - Drok 1 OutPut on 
+  o,2 - Drok 2 OutPut on 
+  o,3 - All Droks OutPut on 
+  */
+  if (cmd == 'o'){    //Turn on the output of droks 
+    switch (cmd_parameter[0]){
+    case 0:
+        Serial1.println("awo1");
+        delay(50);
+        while (Serial1.available() > 0) {
+          Serial1.read();
+        }
+      break;
+    case 1:
+        Serial2.println("awo1");
+        delay(50);
+        while (Serial2.available() > 0) {
+          Serial2.read();
+        }
+    break;
+    case 2:
+        Serial3.println("awo1");
+        delay(50);
+        while (Serial3.available() > 0) {
+          Serial3.read();
+        }  
+    break;
+    case 3:
+      Serial1.println("awo1");
+      delay(50);
+      while (Serial1.available() > 0) {
+        Serial1.read();
+      }
+      Serial2.println("awo1");
+      delay(50);
+      while (Serial2.available() > 0) {
+        Serial2.read();
+      }
+      Serial3.println("awo1");
+      delay(50);
+      while (Serial3.available() > 0) {
+        Serial3.read();
+      }
+    break;
+
+    default:
+    // invalid 
+    Serial.println("invalid argument");
+      break;
     }
   }
+
+  /* 
+  cmd_parameter[subCommand]
+  f,0 - Drok 0 OutPut off 
+  f,1 - Drok 1 OutPut off 
+  f,2 - Drok 2 OutPut off 
+  f,3 - All Droks OutPut off 
+  */
+  if (cmd == 'f'){    //Turn off the output of droks 
+    switch (cmd_parameter[0]){
+    case 0:
+        Serial1.println("awo0");
+        delay(50);
+        while (Serial1.available() > 0) {
+          Serial1.read();
+        }
+      break;
+    case 1:
+        Serial2.println("awo0");
+        delay(50);
+        while (Serial2.available() > 0) {
+          Serial2.read();
+        }
+    break;
+    case 2:
+        Serial3.println("awo0");
+        delay(50);
+        while (Serial3.available() > 0) {
+          Serial3.read();
+        }  
+    break;
+    case 3:
+      Serial1.println("awo0");
+      delay(50);
+      while (Serial1.available() > 0) {
+        Serial1.read();
+      }
+      Serial2.println("awo0");
+      delay(50);
+      while (Serial2.available() > 0) {
+        Serial2.read();
+      }
+      Serial3.println("awo0");
+      delay(50);
+      while (Serial3.available() > 0) {
+        Serial3.read();
+      }
+    break;
+
+    default:
+    // invalid 
+    Serial.println("invalid argument");
+      break;
+    }
+  }
+
+
 
 }
 
@@ -70,11 +193,11 @@ void read_command () {
     //  Otherwise, default to 0
     switch (cmd) {
 
-      case 't': //testing
-      case 'a': // Status
-      case 'o': // Voltage on
-      case 'f': // Voltage off
-      case 'i': // Set all voltage/current limits
+      case 'o': // OutPut On
+      case 'f': // OutPut Off
+      case 's': // Status 
+      case 'v': // Set Voltage
+      case 'c': // Set Current  
         if (debugging) {
           Serial.print("Receive control command: ");
           Serial.println(cmd);
