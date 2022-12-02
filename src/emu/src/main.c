2273537ee7c775f17e665f441f346cacfe8230dd
Sagar Patil <sagarreddypatil@gmail.com>
2022-12-01 22:18:10 -0500

    hello world

--- src/emu/src/main.c
+++ src/emu/src/main.c
@@ -1,7 +1,14 @@
 #include <example.h>
 #include <pico/stdlib.h>
+#include <stdio.h>
+
+int lmao = 0;
 
 int main() {
-  int out = add(2, 3);
-  return out;
+  stdio_init_all();
+  while (true) {
+    printf("Hello, world %d!\n", lmao++);
+    sleep_ms(1000);
+  }
+  return 0;
 }
