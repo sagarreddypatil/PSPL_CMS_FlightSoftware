14e0211095408857b3edaf8fc70b4969422c6163
Sagar Patil <sagarreddypatil@gmail.com>
2022-12-21 01:30:55 -0500

    more flash code

--- lib/w25n01/w25n01.h
+++ lib/w25n01/w25n01.h
@@ -57,10 +57,6 @@ uint8_t w25n01_read_status_register(SPI_DEVICE_PARAM, uint8_t addr);
 void bbm_add(SPI_DEVICE_PARAM, uint16_t logical_block_address, uint16_t physical_block_address);  // note it's block address, not page address, needs write enable
 void bbm_read(SPI_DEVICE_PARAM, w25n01_bbm_entry* entries);
 
-static inline bool busy(SPI_DEVICE_PARAM) {
-  return w25n01_read_status_register(spi, 0) & 0x01;
-}
-
 // This is used for instructions that only need the instruction byte
 #define __INSTR_SINGLE(name, ins)                      \
   static inline void w25n01_##name(SPI_DEVICE_PARAM) { \
@@ -85,3 +81,49 @@ __INSTR_PAGE(write_page, w25n01_ins_program_execute);  // needs write enable
 __INSTR_PAGE(block_erase, w25n01_ins_block_erase);     // needs write enable
 
 #undef __INSTR_PAGE
+
+/*
+ * Status Register Bitfields
+ */
+
+#define __RREG_BITFIELD(name, addr, mask, shift)                         \
+  static inline uint8_t w25n01_get_##name(SPI_DEVICE_PARAM) {            \
+    return (w25n01_read_status_register(spi, addr) & (mask)) >> (shift); \
+  }
+
+#define __RREG_BITFIELD_BOOL(name, addr, bit)                     \
+  static inline bool w25n01_get_##name(SPI_DEVICE_PARAM) {        \
+    return w25n01_read_status_register(spi, addr) & (1 << (bit)); \
+  }
+
+#define __WREG_BITFIELD_BOOL(name, addr, bit)                          \
+  static inline void w25n01_set_##name(SPI_DEVICE_PARAM, bool value) { \
+    uint8_t reg = w25n01_read_status_register(spi, addr);              \
+    if (value) {                                                       \
+      reg |= (1 << (bit));                                             \
+    } else {                                                           \
+      reg &= ~(1 << (bit));                                            \
+    }                                                                  \
+    w25n01_write_status_register(spi, addr, reg);                      \
+  }
+
+// we're not gonna fuck with any of the OTP lockable areas, so we're not gonna define any of those
+
+// SR-2 (Config Register)
+__RREG_BITFIELD_BOOL(buffer_mode, w25n01_config_register, 3);
+__WREG_BITFIELD_BOOL(buffer_mode, w25n01_config_register, 3);
+
+__RREG_BITFIELD_BOOL(ecc_enable, w25n01_config_register, 4);
+__WREG_BITFIELD_BOOL(ecc_enable, w25n01_config_register, 4);
+
+// SR-3 (Status Register)
+__RREG_BITFIELD_BOOL(busy, w25n01_status_register, 0);
+__RREG_BITFIELD_BOOL(write_enable, w25n01_status_register, 1);
+__RREG_BITFIELD_BOOL(erase_fail, w25n01_status_register, 2);
+__RREG_BITFIELD_BOOL(program_fail, w25n01_status_register, 3);
+__RREG_BITFIELD(ecc_status, w25n01_status_register, 0b110000, 4)
+__RREG_BITFIELD_BOOL(bbm_lut_full, w25n01_status_register, 6);
+
+#undef __WREG_BITFIELD_BOOL
+#undef __RREG_BITFIELD_BOOL
+#undef __RREG_BITFIELD
