24eca4eee43f2ebc25f96cfc4c8cbcb7acc306ac
sagarreddypatil <sagarreddypatil@gmail.com>
2023-04-22 17:03:19 -0400

    more refactor

--- src/emu/emu.h
+++ src/emu/emu.h
@@ -2,6 +2,9 @@
 #include <w5500.h>
 #include <commandnet.h>
 
+//------------Devices------------
+SPI_DEVICE(w5500, spi0, 17)
+
 //------------Global Vars------------
 extern int64_t lox_period;
 extern int64_t lox_duty_cycle;
@@ -25,3 +28,10 @@ static const ip_t src_ip      = {192, 168, 2, 50};
 // GPIO
 static const uint LOX_SOLENOID = 26;
 static const uint ETH_SOLENOID = 27;
+
+//------------Tasks------------
+void cmdnet_task_init();
+void cmdnet_task_run();
+
+void solenoid_task_init();
+void solenoid_task_run();
--- src/emu/main.c
+++ src/emu/main.c
@@ -3,10 +3,6 @@
 #include <stdio.h>
 #include <commandnet.h>
 
-//------------Runtime Values and Behaviour------------
-
-SPI_DEVICE(w5500, spi0, 17);
-
 int main() {
   //------------All Initialization------------
   stdio_init_all();
@@ -15,15 +11,6 @@ int main() {
   gpio_set_function(18, GPIO_FUNC_SPI);
   gpio_set_function(19, GPIO_FUNC_SPI);
   gpio_set_function(16, GPIO_FUNC_SPI);
-
-  gpio_init(LOX_SOLENOID);
-  gpio_init(ETH_SOLENOID);
-
-  gpio_put(LOX_SOLENOID, 0);
-  gpio_put(ETH_SOLENOID, 0);
-
-  gpio_set_dir(LOX_SOLENOID, GPIO_OUT);
-  gpio_set_dir(ETH_SOLENOID, GPIO_OUT);
 
   w5500_set(w5500);
   w5500_init(w5500);
@@ -56,58 +43,11 @@ int main() {
          src_mac[2], src_mac[3], src_mac[4], src_mac[5]);
   printf("IP: %d.%d.%d.%d\n", src_ip[0], src_ip[1], src_ip[2], src_ip[3]);
 
-  // Networking
-  tcp_server_t _server;
-  tcp_server_t* server = &_server;
-  tcp_server_init(server, w5500, W5500_S0, 8080);
-
-  // Protocol
-  cmdnet_t _cmdnet;
-  cmdnet_t* cmdnet = &_cmdnet;
-  cmdnet_init(cmdnet, server, cmds, cmds_len, vars, vars_len);
-
-  int64_t eth_cycle_start = 0;
-  int64_t lox_cycle_start = 0;
+  cmdnet_task_init();
+  solenoid_task_init();
 
   while (true) {
-    tcp_server_poll(server);
-
-    if (tcp_server_connected(server)) {
-      cmdnet_handle(cmdnet);
-    }
-
-    int64_t solenoid_time = time_us_64();
-    int64_t on_time       = eth_period * eth_duty_cycle / 100000;
-
-    if (eth_run) {
-      if (eth_cycle_start + eth_period <= solenoid_time) {
-        eth_cycle_start = solenoid_time;
-      }
-
-      if (eth_cycle_start + on_time <= solenoid_time) {
-        gpio_put(ETH_SOLENOID, 0);
-      } else {
-        gpio_put(ETH_SOLENOID, 1);
-      }
-    } else {
-      gpio_put(ETH_SOLENOID, 0);
-    }
-
-    solenoid_time = time_us_64();
-    on_time       = lox_period * lox_duty_cycle / 100000;
-
-    if (lox_run) {
-      if (lox_cycle_start + lox_period <= solenoid_time) {
-        lox_cycle_start = solenoid_time;
-      }
-
-      if (lox_cycle_start + on_time <= solenoid_time) {
-        gpio_put(LOX_SOLENOID, 0);
-      } else {
-        gpio_put(LOX_SOLENOID, 1);
-      }
-    } else {
-      gpio_put(LOX_SOLENOID, 0);
-    }
+    cmdnet_task_run();
+    solenoid_task_run();
   }
 }
