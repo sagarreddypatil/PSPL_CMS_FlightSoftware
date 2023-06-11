c5c3fb2ae3890aaa8c62fcf8626bca7b3c889784
sagarreddypatil <sagarreddypatil@gmail.com>
2023-06-10 19:08:59 -0700

    add pico bootsel script

---
+++ pico.sh
@@ -0,0 +1,24 @@
+RUN=15 # active low, pull low to reset
+BOOTSEL=14 # active low, pull low to bootsel
+
+# configure pins as output
+raspi-gpio set $RUN op
+raspi-gpio set $BOOTSEL op
+
+# Base state
+raspi-gpio set $RUN dh
+raspi-gpio set $BOOTSEL dh
+
+# Enable bootsel
+raspi-gpio set $BOOTSEL dl
+sleep 0.1
+
+# Reset the board
+raspi-gpio set $RUN dl
+sleep 0.5
+raspi-gpio set $RUN dh
+sleep 0.5
+
+# Set bootsel to base state
+raspi-gpio set $BOOTSEL dh
+
