3eaa7953c1ee45c6aa848a2047187d7332bb2680
IanBruch <ibruch@purdue.edu>
2023-10-24 20:05:23 -0400

    Populated t0 prior to sending packet

--- lib/ntp/ntp.c
+++ lib/ntp/ntp.c
@@ -19,6 +19,16 @@ uint64_t ntp_time_to_us(ntp_time_t ntp_time) {
   return time_us;
 }
 
+ntp_time_t us_to_ntp_time(uint64_t micros) {
+  uint32_t seconds    = micros / 1000000;
+  ntp_time_t ntp_time = {
+      .fraction = (uint32_t)(((uint64_t)(UINT32_MAX) * (micros % 1000000ull)) /
+                             1000000ull),
+      .seconds  = seconds};
+
+  return ntp_time;
+}
+
 int64_t get_server_time(SPI_DEVICE_PARAM, ip_t server_addr,
                         w5500_socket_t socket) {
   ntp_packet_t packet = {0};
@@ -29,8 +39,11 @@ int64_t get_server_time(SPI_DEVICE_PARAM, ip_t server_addr,
   w5500_write(spi, socket, W5500_Sn_DIPR0, server_addr, 4);
   w5500_write16(spi, socket, W5500_Sn_DPORT0, NTP_PORT);
 
+  uint64_t t0       = time_us_64();  // t0
+  ntp_time_t t0_ntp = us_to_ntp_time(t0);
+  packet.org        = t0_ntp;
+
   w5500_write_data(spi, socket, &packet, sizeof(ntp_packet_t));
-  uint64_t t0 = time_us_64();  // t0
 
   int read    = 0;
   uint64_t t3 = 0;
@@ -57,20 +70,22 @@ int64_t get_server_time(SPI_DEVICE_PARAM, ip_t server_addr,
 
   uint64_t t1 = ntp_time_to_us(packet.rec);
   uint64_t t2 = ntp_time_to_us(packet.xmt);
- 
+
   uint64_t server_time_center          = (t1 + t2) / 2;
   uint64_t local_time_at_server_center = (t0 + t3) / 2;
 
-  int64_t temp_offset = t2 - t3; //While t1 is not working, use this calculation 
+  int64_t temp_offset =
+      t2 - t3;  // While t1 is not working, use this calculation
   int64_t offset = server_time_center - local_time_at_server_center;
 
   printf("t0:  %lld\n", t0);
-  printf("t1: %lld\n", t1); //t1 receives a 0 input, so returns the - conversion from the year 1900
-  printf("t2: %lld\n", t2); //t2 returns the proper number (current time)
-  printf("t3: %lld\n", t3); //t0 and t3 return proper numbers (system time since start, but called at different times)
-  //printf("%lld  -  ", t1 + t2);
-  //printf("%lld  = 2*  ", t0 + t3);
+  printf("t1: %lld\n", t1);  // t1 receives a 0 input, so returns the -
+                             // conversion from the year 1900
+  printf("t2: %lld\n", t2);  // t2 returns the proper number (current time)
+  printf("t3: %lld\n", t3);  // t0 and t3 return proper numbers (system time
+                             // since start, but called at different times)
+  // printf("%lld  -  ", t1 + t2);
+  // printf("%lld  = 2*  ", t0 + t3);
 
   return temp_offset;
 }
-
