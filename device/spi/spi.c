96a5b5d7259cb5113ea0d7d58db19b9e48055233
Charlie Schoettle <cschoettle981@gmail.com>
2023-10-31 02:02:31 -0400

    working spi_write fucking finally

--- device/spi/spi.c
+++ device/spi/spi.c
@@ -124,9 +124,7 @@ void spi_write_read8(spi_device_t *device, uint8_t *src, uint8_t *dst, size_t si
 		dma_channel_set_config(device->rx_dma, &device->rx_dma_config, false);
 
 		// gpio_put(device->cs_gpio, 0);
-		// dma_start_channel_mask((1u << device->tx_dma) | (1u << device->rx_dma));
-		dma_channel_start(device->tx_dma);
-		dma_channel_start(device->rx_dma);
+		dma_start_channel_mask((1u << device->tx_dma) | (1u << device->rx_dma));
 		// gpio_put(device->cs_gpio, 1);
 }
 
@@ -136,25 +134,24 @@ void spi_write(spi_device_t *device, uint8_t *src, size_t size){
 		channel_config_set_dreq(&device->rx_dma_config, DREQ_FORCE);
 
 		channel_config_set_transfer_data_size(&device->rx_dma_config, DMA_TRANSFER_SIZE);
+		channel_config_set_transfer_data_size(&device->tx_dma_config, DMA_TRANSFER_SIZE);
 
 		// Makes DMA move it's read address by DMA_TRANSFER_SIZE after each transfer
 		channel_config_set_read_increment(&device->tx_dma_config, true); //default value
 		channel_config_set_write_increment(&device->tx_dma_config, true); 
+		channel_config_set_read_increment(&device->rx_dma_config, true); //default value
+		channel_config_set_write_increment(&device->rx_dma_config, true); 
+		dma_channel_set_config(device->tx_dma, &device->tx_dma_config, false);
+		dma_channel_set_config(device->rx_dma, &device->rx_dma_config, false);
 		
 		// Sets number of DMA transfers to do
 		dma_channel_set_trans_count(device->tx_dma, size, false);
+		dma_channel_set_trans_count(device->rx_dma, size, false);
 		// Sets address to transfer from
 
 		// gpio_put(device->cs_gpio, 0);
 
-
-		dma_channel_set_config(device->tx_dma, &device->tx_dma_config, false);
-		dma_channel_set_config(device->rx_dma, &device->rx_dma_config, false);
-		dma_channel_set_read_addr(device->tx_dma, src, false);
-		dma_channel_start(device->tx_dma);
-
-		printf("waiting on dma channel to complete\n");
-		// dma_channel_wait_for_finish_blocking(device->tx_dma);
+		dma_start_channel_mask((1u << device->tx_dma) | (1u << device->rx_dma));
 
 		// gpio_put(device->cs_gpio, 1);
 }
--- src/spi-dma-test/main.c
+++ src/spi-dma-test/main.c
@@ -9,10 +9,10 @@
 
 spi_device_t test_device = {
   .spi_inst = spi0,
-  .miso_gpio = 0,
-  .mosi_gpio = 0,
-  .sck_gpio = 0,
-  .cs_gpio = 0,
+  .miso_gpio = 18,
+  .mosi_gpio = 19,
+  .sck_gpio = 16,
+  .cs_gpio = 17,
   .baudrate = 9600
   };
 
@@ -23,33 +23,33 @@ int main()
     ;
     printf("\n\nstdio connected!\n");
 
-    // spi_device_init(&test_device);
+    spi_device_init(&test_device);
 
     uint8_t tx_src[4] = {1,2,3,4};
-    uint8_t tx_dst[4];  // SPI FIFO (TX)
+    uint8_t tx_dst[4];              // SPI FIFO (TX)
     uint8_t rx_src[4] = {5,6,7,8};  // SPI FIFO (RX)
     uint8_t rx_dst[4] = {0,0,0,0};
 
-    // dma_channel_configure(
-	// 	test_device.tx_dma,
-	// 	&test_device.tx_dma_config,
-	// 	tx_dst,
-	// 	tx_src,
-	// 	4,
-	// 	false 
-	// );
-    // dma_channel_configure(
-	// 	test_device.rx_dma,
-	// 	&test_device.rx_dma_config,
-	// 	rx_dst,
-	// 	rx_src,
-	// 	4,
-	// 	false 
-	// );
+    dma_channel_configure(
+		test_device.tx_dma,
+		&test_device.tx_dma_config,
+		tx_dst,
+		tx_src,
+		4,
+		false 
+	);
+    dma_channel_configure(
+		test_device.rx_dma,
+		&test_device.rx_dma_config,
+		rx_dst,
+		rx_src,
+		4,
+		false 
+	);
 
     printf("DMA configured!\n");
 
-    dma_move(tx_src, tx_dst, sizeof(tx_src));
+    spi_write(&test_device, tx_src, sizeof(tx_src));
 
     printf("TX DST: ");
     for (size_t i = 0; i < 4; i++)
