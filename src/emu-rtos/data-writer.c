db7c07a748f3825f76600b548951f985b35a4acb
Sagar Patil <sagarreddypatil@gmail.com>
2024-01-05 00:38:34 +0000

    make emu-rtos build after spi change

--- src/emu-rtos/data-writer.c
+++ src/emu-rtos/data-writer.c
@@ -3,11 +3,10 @@
 QueueHandle_t dataOutputQueueHandle;
 
 void data_writer_task() {
-    w5500_create_udp_socket(&w5500, SENSORNET_SOCKET, 5001, false, false,
-                            false);
+    w5500_create_udp_socket(&eth0, SENSORNET_SOCKET, 5001, false, false, false);
 
-    w5500_write(&w5500, SENSORNET_SOCKET, W5500_Sn_DIPR0, SENSORNET_IP, 4);
-    w5500_write16(&w5500, SENSORNET_SOCKET, W5500_Sn_DPORT0, SENSORNET_PORT);
+    w5500_write(&eth0, SENSORNET_SOCKET, W5500_Sn_DIPR0, SENSORNET_IP, 4);
+    w5500_write16(&eth0, SENSORNET_SOCKET, W5500_Sn_DPORT0, SENSORNET_PORT);
 
     while (true) {
         sensornet_packet_t packet;
@@ -18,7 +17,7 @@ void data_writer_task() {
 
         // send the packet
         w5500_error_t status = w5500_write_data(
-            &w5500, SENSORNET_SOCKET, &packet, sizeof(sensornet_packet_t));
+            &eth0, SENSORNET_SOCKET, &packet, sizeof(sensornet_packet_t));
 
         if (status < 0) {
             // insufficient space in socket, put the packet back in the queue
